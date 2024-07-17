e729f8eb656dd15f3033e1907714f91f335086a9
alekanic <koirallabublik@gmail.com>
2024-07-17 17:10:01 +0300

    version 1

--- main.py
+++ main.py
@@ -1,25 +1,71 @@
+import clouds
 from map import Map
-import time
-import os
+from clouds import Clouds
+import time # библиотека чтобы вызвать time.sleep()
+import os # для очистки консоли
+from helicopter import Helicopter as Helico
+from pynput import keyboard
+import json
 
+# объявляем константы
 TICK_SLEEP = 0.05
 TREE_UPDATE = 50
-FIRE_UPDATE = 25
+FIRE_UPDATE = 40
+CLOUDS_UPDATE = 100
 MAP_W, MAP_H = 20, 10
+tick = 1
 
 field = Map(MAP_W, MAP_H)
-field.generate_forests(3, 8)
-field.generate_rivers(8)
+helico = Helico(MAP_W, MAP_H)
+clouds = Clouds(MAP_W, MAP_H)
 
-# то, какой кадр отрисовывается
-tick = 1
+MOVES = {"w": (-1, 0), "d": (0, 1), "s": (1, 0), "a": (0, -1)}
+# f - save, g - recover
+
+def process_key(key):
+    global helico, tick, clouds, field
+    c = key.char.lower()
+    # обработка движений вертолета
+    if c in MOVES.keys():
+        dx = MOVES[c][0]
+        dy = MOVES[c][1]
+        helico.move(dx, dy)
+    # сохранение игры
+    elif c == 'f':
+        data = {"helicopter": helico.export_data(),
+                "clouds": clouds.export_data(),
+                "field": field.export_data(),
+                "data": tick}
+        with open("level.json", "w") as lvl:
+            json.dump(data, lvl)
+    # загрузка игры
+    elif c == 'g':
+        with open("level.json", "r") as lvl:
+            data = json.load(lvl)
+            helico.import_data(data["helicopter"])
+            tick = data["tick"] or 1
+            field.import_data(data["field"])
+            clouds.import_data(data["clouds"])
+
+
+
+
+listener = keyboard.Listener(
+    on_press=None,
+    on_release=process_key,)
+listener.start()
+
 while True:
-    os.system("clear")
-    print("TICK" + str(tick))
-    field.print_map()
+    os.system("clear") #cls
+    field.process_helicopter(helico, clouds)
+    helico.print_stats()
+    field.print_map(helico, clouds)
+    print("TICK" + str(tick))  # отрисовываем номер кадра, который сейчас отрисовыватеся
     tick += 1
-    time.sleep(TICK_SLEEP)
+    time.sleep(TICK_SLEEP) # ограничение частоты отрисовки кадров
     if (tick % TREE_UPDATE == 0):
         field.generate_tree()
     if (tick % FIRE_UPDATE == 0):
         field.update_fires()
+    if (tick % CLOUDS_UPDATE == 0):
+        clouds.update_clouds()
--- map.py
+++ map.py
@@ -1,15 +1,50 @@
 from utilis import randbool
 from utilis import randcell
 from utilis import randneighbour
+import os
 
 # 0 - поле
 # 1 - дерево
 # 2 - река
 # 3 - госпиталь
 # 4 - апгрейд-центр
+# 5 - огонь
 
 CELL_TYPES = "🟩🌲🌊🚑🏣🔥"
+THREE_BONUS = 100
+UPGRADE_COST = 5000
+LIFE_COST = 10000
 class Map:
+
+    def __init__(self, w, h):
+        self.w = w
+        self.h = h
+        self.cells = [[0 for i in range(w)] for j in range(h)]
+        self.generate_forests(3, 10)
+        self.generate_rivers(10)
+        self.generate_upgrade_shop()
+
+    def check_bound(self, x, y):
+        if (x < 0 or y < 0 or x >= self.h or y >= self.w):
+            return False
+        return True
+
+    def print_map(self, helico, clouds):
+        print("⬛️" * (self.w + 2))
+        for ri in range(self.h):
+            print("⬛", end="")
+            for ci in range(self.w):
+                cell = self.cells[ri][ci]
+                if (clouds.cells[ri][ci] == 1):
+                    print("☁️", end="")
+                elif (clouds.cells[ri][ci] == 2):
+                    print("🌩", end="")
+                elif (helico.x == ri and helico.y == ci):
+                    print("🚁", end="")
+                elif (cell >= 0 and cell < len(CELL_TYPES)):
+                    print(CELL_TYPES[cell], end="")
+            print("⬛")
+        print("⬛️" * (self.w + 2))
 
     def generate_rivers(self, l):
         rc = randcell(self.w, self.h)
@@ -29,36 +64,24 @@ class Map:
                 if randbool(r, mxr):
                     self.cells[ri][ci] = 1
 
-    def print_map(self):
-        print("⬛️" * (self.w + 2))
-        for row in self.cells:
-            print("⬛", end='')
-            for cell in row:
-                if cell >= 0 and cell < len(CELL_TYPES):
-                    print(CELL_TYPES[cell], end='')
-                # if cell == 0:
-                #     print("🟩", end='')
-                # elif cell == 1:
-                #     print("🌲", end='')
-                # elif cell == 2:
-                #     print("🌊", end='')
-                # elif cell == 3:
-                #     print("🚑", end='')
-                # elif cell == 4:
-                #     print("🏣", end='')
-            print("⬛️")
-        print("⬛️" * (self.w + 2))
-
-    def check_bound(self, x, y):
-        if x < 0 or y < 0 or x > self.h or y > self.w:
-            return False
-        return True
-
     def generate_tree(self):
         c = randcell(self.w, self.h)
         cx, cy = c[0], c[1]
         if (self.check_bound(cx, cy) and self.cells[cx][cy] == 0):
             self.cells[cx][cy] = 1
+
+    def generate_upgrade_shop(self):
+        c = randcell(self.w, self.h)
+        cx, cy = c[0], c[1]
+        self.cells[cx][cy] = 4
+
+    def generate_hospital(self):
+        c = randcell(self.w, self.h)
+        cx, cy = c[0], c[1]
+        if (self.cells[cx][cy] != 4):
+            self.cells[cx][cy] = 3
+        else:
+            self.generate_hospital()
 
     def add_fire(self):
         c = randcell(self.w, self.h)
@@ -66,18 +89,43 @@ class Map:
         if self.cells[cx][cy] == 1:
             self.cells[cx][cy] = 5
 
+    # функция будет удалять огни, когда те уже сгорели
+    # а так же добавлять новые
     def update_fires(self):
         for ri in range(self.h):
             for ci in range(self.w):
                 cell = self.cells[ri][ci]
                 if cell == 5:
-                    self.cells[ri][ci] = 0
-        for i in range(5):
-            self.add_fire()
+                    self.cells[ri][ci] = 0 # удаляем старый пожар
+        for i in range(10):
+            self.add_fire() # начинаем новый пожар
 
+    def process_helicopter(self, helico, clouds):
+        c = self.cells[helico.x][helico.y]
+        d = clouds.cells[helico.x][helico.y]
+        if (c == 2):
+            helico.tank = helico.mxtank
+        if (c == 5 and helico.tank > 0):
+            helico.tank -= 1
+            helico.score += THREE_BONUS
+            self.cells[helico.x][helico.y] = 1
+        if (c == 4 and helico.score >= UPGRADE_COST):
+            helico.mxtank += 1
+            helico.score -= UPGRADE_COST
+        if (c == 3 and helico.score >= LIFE_COST):
+            helico.lives += 1
+            helico.score -= LIFE_COST
+        if (d == 2):
+            helico.lives -= 1
+            if (helico.lives == 0):
+                os.system("clear")
+                print("========================================")
+                print("GAME OVER, YOUR SCORE IS ", helico.score)
+                print("========================================")
+                exit(0)
 
+    def export_data(self):
+        return {"cells": self.cells}
 
-    def __init__(self, w, h):
-        self.w = w
-        self.h = h
-        self.cells = [[0 for i in range(w)] for i in range(h)]
+    def import_data(self, data):
+        self.cells = data["cells"] or [[0 for i in range(self.w)] for j in range(self.h)]
